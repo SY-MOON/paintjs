1954be2fd66adcf591fd4a1b031bf772c55740a0
SY-MOON <seokyoung.moon90@gmail.com>
2020-05-14 23:25:18 +0900

    [ADD] - 브러쉬 컬러 변경 - 브러쉬 굵기 변경 - 페인트통 클릭시 배경색 채우기 - 이미지 저장하기

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
+const colors =  document.getElementsByClassName("control__color");
+const range = document.getElementById("range");
+const mode = document.getElementById("btn-fill");
+const saveBtn = document.getElementById("btn-save");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c"
+const CANVAS_SIZE = 700
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
 	painting = false;
@@ -33,10 +42,71 @@ function onMouseDown(e) {
 	painting = true;
 }
 
+function handleColorClick(e) {
+	const color = e.target.style.backgroundColor;
+	ctx.strokeStyle = color;
+	ctx.fillStyle = color;
+}
+
+function handleRangeChange(e) {
+	const size = e.target.value;
+	ctx.lineWidth = size;
+}
+
+function handleModeClick(e) {
+	if(filling == true) {
+		filling = false;
+		mode.innerText = "Fill"
+	} else {
+		filling = true;
+		mode.innerText = "Paint";
+	}
+} 
+
+function handleCanvasClick() { 
+	if(filling) {
+		ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE)
+	}
+}
+
+function handleRightClick(e){
+	e.preventDefault();
+}
+
+function handleSaveBtn() {
+	const image = canvas.toDataURL("image/jpeg");
+	const link = document.createElement("a");
+	console.log('image', image);
+	console.log('link', link);
+	link.href = image;
+	link.download = "paint";
+	link.click();
+}
 
 if(canvas) {
 	canvas.addEventListener("mousemove", onMouseMove)
 	canvas.addEventListener("mousedown", startPainting)
 	canvas.addEventListener("mouseup", stopPainting)
 	canvas.addEventListener("mouseleave", stopPainting)
+	canvas.addEventListener("click", handleCanvasClick)
+	canvas.addEventListener("contextmenu", handleRightClick)
+	
+}
+
+
+Array.from(colors).forEach(color => {
+	color.addEventListener("click", handleColorClick)
+});
+
+
+if(range) {
+	range.addEventListener("click", handleRangeChange)
+}
+
+if(mode) {
+	mode.addEventListener("click", handleModeClick)
+}
+
+if(saveBtn) {
+	saveBtn.addEventListener("click", handleSaveBtn)
 }
